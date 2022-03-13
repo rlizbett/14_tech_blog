e62b5b15ce014ce1ed491526beb796527e112800
lizbett <rlizbett21@gmail.com>
2022-03-12 21:57:41 -0600

    login/out js added and (still need style.css)

--- models/user.js
+++ models/user.js
@@ -4,31 +4,31 @@ const sequelize = require('../config/connection');
 class User extends User { }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {isEmail: true,},
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {len: [6],},
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-},
-{
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true, },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [6], },
+    },
+  },
+
+  {
     hooks: {
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
@@ -38,7 +38,6 @@ User.init(
         updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
         return newUserData;
       },
-      
     },
     sequelize,
     timestamps: false,
