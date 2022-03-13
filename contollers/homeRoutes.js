075a270e3be2f10c9140cdeafe4991429ed18ed5
lizbett <rlizbett21@gmail.com>
2022-03-12 20:53:20 -0600

    added homeroutes.js

---
+++ contollers/homeRoutes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const blogPost = postData.map((Post) =>
+            Post.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            blogPost,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: [
+                        'name',
+                    ],
+                },
+            ],
+        });
+
+        const posts = postData.get({ plain: true });
+        res.render('post', { ...posts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
